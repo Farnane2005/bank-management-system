e79f28a547f5ba299f8d7d073b266abc5d0bedd1
NOUBL-Badr <badr.noubl123@gmail.com>
2026-06-06 17:07:03 +0100

    adding deployement detail and some minor changes

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,17 +1,24 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import (
     declarative_base,
     sessionmaker
 )
 
-DATABASE_URL = "sqlite:///./bank.db"
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./bank.db")
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={
-        "check_same_thread": False
-    }
-)
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(
+        DATABASE_URL,
+        connect_args={
+            "check_same_thread": False
+        }
+    )
+else:
+    engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
     autocommit=False,
